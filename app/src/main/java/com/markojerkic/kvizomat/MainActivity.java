3e525dec6953b77402ca0c627fb5f0946b54c4d5
marko <markojerkic266@gmail.com>
2020-03-13 14:52:37 +0100

    Prijava gotova

--- app/src/main/java/com/markojerkic/kvizomat/MainActivity.java
+++ app/src/main/java/com/markojerkic/kvizomat/MainActivity.java
@@ -1,9 +1,16 @@
 package com.markojerkic.kvizomat;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
@@ -12,13 +19,29 @@ import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
 import androidx.navigation.ui.NavigationUI;
 
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.navigation.NavigationView;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.squareup.picasso.Picasso;
+
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final int RC_SIGN_IN = 555;
     private AppBarConfiguration mAppBarConfiguration;
+    private FirebaseUser mUser;
+    private TextView userName;
+    private TextView userEmail;
+    private ImageView userPhoto;
+    private TextView odjavaView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +49,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -34,8 +61,6 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
-        NavigationView navigationView = findViewById(R.id.nav_view);
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
@@ -46,6 +71,76 @@ public class MainActivity extends AppCompatActivity {
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
 
+        userName = navigationView.getHeaderView(0).findViewById(R.id.user_name);
+        userEmail = navigationView.getHeaderView(0).findViewById(R.id.user_email);
+        userPhoto = navigationView.getHeaderView(0).findViewById(R.id.user_photo);
+        odjavaView = navigationView.findViewById(R.id.odjava);
+
+        if (FirebaseAuth.getInstance().getCurrentUser() == null) {
+            Log.d("auth", "nema usera");
+            createSignInIntent();
+        } else {
+            mUser = FirebaseAuth.getInstance().getCurrentUser();
+            updateUI();
+        }
+
+        odjavaView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (mUser != null) {
+                    odjava();
+                }
+            }
+        });
+
+    }
+
+    private void odjava() {
+        AuthUI.getInstance().signOut(this).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                createSignInIntent();
+            }
+        });
+    }
+
+    private void updateUI() {
+        String name = mUser.getDisplayName();
+        String email = mUser.getEmail();
+        Uri photoURL = mUser.getPhotoUrl();
+        Log.d("auth", "ime: " + userName);
+        Log.d("auth", "email " + userEmail);
+        Log.d("auth", "photo " + photoURL);
+        userEmail.setText(email);
+        userName.setText(name);
+        Picasso.get().load(photoURL).into(userPhoto);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+            if (resultCode == RESULT_OK) {
+                mUser = FirebaseAuth.getInstance().getCurrentUser();
+                updateUI();
+            } else {
+                int errorCode = response.getError().getErrorCode();
+                Log.e("Kviz", "signin greška " + errorCode);
+            }
+        }
+    }
+
+    public void createSignInIntent() {
+        // Firebase login
+        List<AuthUI.IdpConfig> providers = Arrays.asList(
+//                new AuthUI.IdpConfig.FacebookBuilder().build(),
+                new AuthUI.IdpConfig.GoogleBuilder().build()
+        );
+
+        startActivityForResult(AuthUI.getInstance().createSignInIntentBuilder()
+                .setAvailableProviders(providers).build(), RC_SIGN_IN);
     }
 
     @Override
